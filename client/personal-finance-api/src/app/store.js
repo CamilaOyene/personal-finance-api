63ef6266f605eb74c7e88e3747afdab091801fe2
Camila Oyene <coyene63@gmail.com>
2025-06-21 01:11:07 -0300

    slices conectados al store

--- client/personal-finance-api/src/app/store.js
+++ client/personal-finance-api/src/app/store.js
@@ -1,9 +1,15 @@
 import { configureStore } from '@reduxjs/toolkit';
 //Importo reducer de slice de autenticación
 import authReducer from '../features/auth/authSlice';
+import accountsReducer from '../features/accounts/accountsSlice';
+import categoriesReducer from '../features/categories/categoriesSlice';
+import transactionsReducer from '../features/transactions/transactionsSlice';
 
 export const store = configureStore({
     reducer: {
         auth: authReducer,
+        accounts: accountsReducer,
+        categories: categoriesReducer,
+        transactions: transactionsReducer
     },
 });
