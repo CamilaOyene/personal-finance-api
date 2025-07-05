561f1412ed366f01f79db05eaa698d1878dcdb48
Camila Oyene <coyene63@gmail.com>
2025-07-04 21:51:29 -0300

    ttabla de ultimas 5 transaccione

---
+++ client/personal-finance-api/src/components/dashboard/TransactionsTableDashboard.jsx
@@ -0,0 +1,57 @@
+import { Card, Table } from 'antd';
+import { useSelector } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+import dayjs from 'dayjs';
+
+const TransacionsTableDashboard = () => {
+    const { latestTransactions, loading } = useSelector((state) => state.dashboard);
+    const navigate = useNavigate();
+
+
+    //Columnas de la tabla 
+    const columns = [
+        {
+            title: 'Descripción',
+            dataIndex: 'description',
+            key: 'description',
+        },
+        {
+            title: 'Monto',
+            dataIndex: 'amount',
+            key: 'amount',
+            render: (amount, record) => (
+                <span style={{ color: record.type === 'income' ? 'green' : 'red' }}>
+                    ${amount}
+                </span>
+            ),
+        },
+        {
+            title: 'Tipo',
+            dataIndex: 'type',
+            key: 'type',
+            render: (type) => (
+                <Tag color={type === 'income' ? 'green' : 'red'}>
+                    {type === 'income' ? 'Ingreso' : 'Gasto'}
+                </Tag>
+            ),
+        },
+        {
+            title: 'Fecha',
+            dataIndex: 'date',
+            key: 'date',
+            render: (date) => dayjs(date).format('DD/MM/YYYY'),
+        },
+    ];
+
+    return (
+        <Card title='Últimas transacciones' >
+            <Table columns={columns} dataSource={latestTransactions} rowKey={'_id'} loading={loading} pagination={false} />
+            <div style={{ marginTop: 16, textAlign: 'right' }}>
+                <Button type='link' onClick={() => navigate('/transactions')}>
+                    Ver más
+                </Button>
+            </div>
+        </Card>
+    );
+};
+export default TransacionsTableDashboard;
